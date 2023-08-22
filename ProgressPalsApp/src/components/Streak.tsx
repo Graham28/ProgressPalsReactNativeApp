32a79ad1f12ae65ed932261bf244e1f0d7c7619e
Graham28 <graham28292@gmail.com>
2023-08-22 21:34:34 +0100

    Streak updates

--- ProgressPalsApp/src/components/Streak.tsx
+++ ProgressPalsApp/src/components/Streak.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
 export type StreakProps = {
     title: string;
@@ -8,16 +8,26 @@ export type StreakProps = {
 };
 
 const Streak: React.FC<StreakProps> = ({ title, unit, streakLength }) => {
+    const isCurrentUser = true;
+    const lastUpdatedDate = new Date();
+    lastUpdatedDate.setDate(lastUpdatedDate.getDate() - 1);
+
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+
+    const canUpdate = isCurrentUser && today > lastUpdatedDate;
     const calculateProgress = () => {
-        if (streakLength < 3) return streakLength / 3;
-        if (streakLength < 7) return streakLength / 7;
-        if (streakLength < 31) return streakLength / 31;
-        if (streakLength < 50) return streakLength / 50;
-        if (streakLength < 100) return streakLength / 100;
-        return 1;  // Full progress
+        const milestones = [3, 7, 31, 50, 100];
+        for (let milestone of milestones) {
+            if (streakLength < milestone) {
+                return streakLength / milestone;
+            }
+        }
+        return 1;
     };
 
     const progressBarFlexValue = calculateProgress();
+    const [showOptions, setShowOptions] = useState(false);
 
     return (
         <View style={streakStyles.container}>
@@ -29,31 +39,42 @@ const Streak: React.FC<StreakProps> = ({ title, unit, streakLength }) => {
                 <View style={[streakStyles.progressBar, { flex: progressBarFlexValue }]} />
                 <Text style={[streakStyles.progressEmoji, { left: `${calculateProgress() * 100}%` }]}>🏃‍♀️</Text>
             </View>
+            {canUpdate && (
+                    <View style={streakStyles.buttonContainer}>
+                        <TouchableOpacity onPress={() => { /* Logic to end the streak can go here */ }}>
+                            <Text style={streakStyles.buttonText}>Reset 🌱</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={() => { /* Logic to add a day can go here */ }}>
+                            <Text style={streakStyles.buttonText}>Still going 💪</Text>
+                        </TouchableOpacity>
+                    </View>
+                )}
         </View>
     );
 }
 
+
+
 const streakStyles = StyleSheet.create({
     container: {
-        flexDirection: 'row',
+        flexDirection: 'column',  // Changed to column
         justifyContent: 'space-between',
         alignItems: 'center',
-        borderRadius: 10,    // Soft rounded edges
-        padding: 20,         // Increased padding for spacious feel
-        paddingBottom: 40,   // More space for the progress bar
+        borderRadius: 10,    
+        padding: 20,         
+        paddingBottom: 15,   // Increased padding for buttons
         marginTop: 15,
-        marginBottom: 10,    // Some margin at the bottom so the shadow isn't cut off
-        backgroundColor: '#C5E6DB',  // Light background complementary to the gradient
+        marginBottom: 10,    
+        backgroundColor: '#C5E6DB', 
 
-        // Shadow properties for floating effect
         shadowColor: "#000",
         shadowOffset: {
             width: 0,
-            height: 5,        // More vertical offset for the shadow
+            height: 5,
         },
-        shadowOpacity: 0.12,  // Slightly softer shadow
+        shadowOpacity: 0.12,
         shadowRadius: 3.84,   
-        elevation: 5,        // Elevation for Android
+        elevation: 5,        
     },
     title: {
         fontSize: 18,
@@ -72,16 +93,13 @@ const streakStyles = StyleSheet.create({
         fontWeight: '600',
     },
     progressBarContainer: {
-        position: 'absolute',
-        bottom: 20, 
-        left: 20,  
-        right: 20,
+        width: '100%', // Take full width of parent
+        marginTop: 20, 
         height: 10, 
         flexDirection: 'row',  
         backgroundColor: '#E0E0E0',
         borderRadius: 5,  
 
-        // Reduced shadow properties for the inner progress bar
         shadowColor: "#000",
         shadowOffset: {
             width: 0,
@@ -91,7 +109,6 @@ const streakStyles = StyleSheet.create({
         shadowRadius: 1.21,
         elevation: 2,
     },
-    
     progressBar: {
         height: 10,
         borderRadius: 5,  
@@ -102,7 +119,23 @@ const streakStyles = StyleSheet.create({
         bottom: -11,           
         fontSize: 30,          
         transform: [{ translateX: -18 },{ scaleX: -1 }],
-    },   
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: '100%', // Take full width of the parent
+        marginTop: 13,
+        paddingHorizontal: 0,  // Add some horizontal padding
+    },
+    buttonText: {
+        fontSize: 16, // Reduced size for clarity
+        paddingHorizontal: 10,
+        paddingVertical: 5,
+        backgroundColor: '#112A46',  // A background for the buttons
+        color: 'white',              // White text for contrast
+        borderRadius: 5,             // Rounded corners for the buttons
+    }
+    
 });
 
 export default Streak;
